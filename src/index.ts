97f1e8404c76d80200a42e8e7ece59380f7eaf3b
nicholas houlding <nih642@gmail.com>
2026-03-28 21:01:18 -0700

    Cleaning up initial project structure; Added initial color palette

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,12 @@
 import { serve } from "bun";
-import index from "./index.html";
+import index from "../index.html";
 
 const server = serve({
-  routes: {
-    // Serve index.html for all unmatched routes.
-    "/*": index,
-  },
-
+  routes: {"/*": index,},
   development: process.env.NODE_ENV !== "production" && {
-    // Enable browser hot reloading in development
     hmr: true,
-
-    // Echo console logs from the browser to the server
     console: true,
   },
 });
 
-console.log(`🚀 Server running at ${server.url}`);
+console.log(`Server running at ${server.url}`);
